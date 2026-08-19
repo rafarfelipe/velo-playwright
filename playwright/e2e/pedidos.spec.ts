e50619a5296b0745690c89cbedc04de197e4da35
Rafael Felipe <rafarfelipe91@gmail.com>
2026-08-19 11:35:05 -0300

    feat: Refatorando e atualizando os testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,23 +1,128 @@
 import { test, expect } from '@playwright/test'
 
+import { generateOrderCode } from '../support/helpers'
+import { number } from 'zod'
+
 /// AAA - Arrange, Act, Assert
-test('Deve consultar um pedido aprovado', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:5173/')
-  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+test.describe('Consulta de Pedido', () => {
+  test.beforeEach(async ({page}) => {
+    // Arrange
+    await page.goto('http://localhost:5173/')
+    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-  // Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-WGILT7')
-  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
-  
-  // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 10_000})
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-WGILT7')
+    await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+    await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+  })
 
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  test('Deve consultar um pedido aprovado', async ({ page }) => {
+    // Test Data
+    const order = {
+      number: 'VLO-WGILT7',
+      status: 'APROVADO',
+      color: 'Lunar White',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'Ravena Felipe',
+        email: 'ravena@velo.dev'
+      },
+      payment: 'À Vista'
+    }
+    
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
+    // Assert
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
+  })
+
+  test('Deve consultar um pedido reprovado', async ({ page }) => {
+    // Test Data
+    const order = {
+      number: 'VLO-TZFCFQ',
+      status: 'REPROVADO',
+      color: 'Midnight Black',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Lebron James',
+        email: 'james@dev.com'
+      },
+      payment: 'À Vista'
+    }
+
+    // Act
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    // Assert
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `)
+  })
+
+  test('Deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
+    const order = generateOrderCode()
+
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    const title = page.getByRole('heading', { name: 'Pedido não encontrado' })
+    await expect(title).toBeVisible()
+
+    const message = page.locator('p', { hasText: 'Verifique o número do pedido e tente novamente' })
+    await expect(message).toBeVisible()
+  })
 })
---
+++ playwright/support/helpers.ts
@@ -0,0 +1,12 @@
+export function generateOrderCode() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let code = '';
+  
+    for (let i = 0; i < 6; i++) {
+      code += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+  
+    return `VLO-${code}`;
+  }
