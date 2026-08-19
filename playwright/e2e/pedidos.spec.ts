3495f49fb5c25f4b3d737f7c0ab14ab800a62d24
Rafael Felipe <rafarfelipe91@gmail.com>
2026-08-18 22:58:42 -0300

    feat: /// AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
 test('Deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-
-  // Checkpoint 1: A página deve estar online
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
-  // Checkpoint 2: Acessar a página de consulta de pedido
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-WGILT7')
-
   await page.getByTestId('search-order-button').click()
   
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-WGILT7')
 
